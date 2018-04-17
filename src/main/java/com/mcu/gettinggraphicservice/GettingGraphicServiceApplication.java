357a710d2b05a15a8ce8a1b169cffba98402f042
Mark Heckler <mark.heckler@gmail.com>
2018-04-17 14:44:35 -0500

    Initial commit, new project

---
+++ src/main/java/com/mcu/gettinggraphicservice/GettingGraphicServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mcu.gettinggraphicservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingGraphicServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingGraphicServiceApplication.class, args);
+	}
+}
